f37ea49ea1b90d8a472dda567a9929abe08ce1b5
fiqri khoirul m <fiqrikm18@gmail.com>
2022-11-15 20:52:47 +0700

    feat: create functional to write the fiel output

--- cmd/commands.go
+++ cmd/commands.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/fiqrikm18/cli/tools/pkg/models"
 	"github.com/spf13/cobra"
 	"log"
 	"os"
@@ -19,7 +20,7 @@ var outputType string
 func Execute() {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Need log directory path parameters.")
 	}
 
 	rootCmd.Flags().StringVarP(&output, "output", "o", homeDir+"/", "choose directory where you want place the output file")
@@ -35,4 +36,38 @@ func runCommand(cmd *cobra.Command, args []string) {
 	if err := cobra.MinimumNArgs(1)(cmd, args); err != nil {
 		log.Fatal(err)
 	}
+
+	content, err := readFile(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch outputType {
+	case "text":
+		err := content.WriteTextFile(output)
+		if err != nil {
+			log.Fatal(err)
+		}
+		break
+	case "json":
+		err := content.WriteJsonFile(output)
+		if err != nil {
+			log.Fatal(err)
+		}
+		break
+	default:
+		log.Fatal("File format not supported")
+	}
+}
+
+func readFile(path string) (*models.Log, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	logData := models.NewLog()
+	logData.Content = string(content)
+
+	return logData, nil
 }
